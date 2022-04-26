2a18ba4efb094da05043c706c2718e0c833ba49f
aman88000 <aman88000@gmail.com>
2022-04-26 23:37:49 +0530

    mapActions use in vueX

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -2,36 +2,40 @@
     <div>
         <h3>Child vue component</h3>
         <p>{{$store.state.title}}</p>
-        <button @click="changeTitle">Change Title</button>
+        <button @click="changeTitle({title: 'test mapActions'})">Change Title</button>
     </div>
 </template>
 
 <script>
-// import {mapstate, mapMutations, mapActions, mapGetters} from 'vuex'
+import {mapActions} from 'vuex'
     export default {
         name: 'ChildVue',
         methods: {
-            changeTitle(){
-              //  this.$store.state.title = 'abcd';     // simple way
-              //  this.$store.commit('setTitle', 'updated Title')     // 1 way
-            //   this.$store.commit('setTitle', {                    // 2 way
-            //       title: 'vuex Title'
-            //   })
 
-            //    this.$store.commit({
-            //        type: 'setTitle',
-            //       title: 'vuex Title'
-            //   })
+            ...mapActions({
+                changeTitle: 'newTitle2'
+            }),
+            // changeTitle(){
+            //   //  this.$store.state.title = 'abcd';     // simple way
+            //   //  this.$store.commit('setTitle', 'updated Title')     // 1 way
+            // //   this.$store.commit('setTitle', {                    // 2 way
+            // //       title: 'vuex Title'
+            // //   })
+
+            // //    this.$store.commit({
+            // //        type: 'setTitle',
+            // //       title: 'vuex Title'
+            // //   })
 
 
-            // this.$store.dispatch({
-            //     type: 'newTitle', title: 'new Action Title'
-            // }).then((res)=> console.log(res))
+            // // this.$store.dispatch({
+            // //     type: 'newTitle', title: 'new Action Title'
+            // // }).then((res)=> console.log(res))
 
-                this.$store.dispatch({
-                type: 'newTitle2', title: 'new Action Title newTitle 2'
-            })
-             }
+            //     this.$store.dispatch({
+            //     type: 'newTitle2', title: 'new Action Title newTitle 2'
+            // })
+            //  }
         }
     }
 </script>
