9572e9c297d75fca7fa0a89ecdb446a1a9c0cec6
aman88000 <aman88000@gmail.com>
2022-04-26 23:35:15 +0530

    mapGetters use in vuex

--- src/components/First.vue
+++ src/components/First.vue
@@ -3,14 +3,14 @@
       <h2>First Parent Component</h2>
       <h2>{{title}}</h2>
       <h3>{{waheGuru}}</h3>
-      <h3>{{WaheGuru1313}}</h3>
+      <h3>{{WaheGuru1313('1313Wahe Guru ')}}</h3>
       <p>{{$store.state.name}}</p>
       <ChildVue />
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import {mapState, mapGetters} from 'vuex'
 import ChildVue from './Child.vue'
 export default {
   name: "FirstVue",
@@ -21,14 +21,19 @@ export default {
       ...mapState({
           title: state => state.name
       }),
-      waheGuru(){
-        //  return this.$store.getters.WaheGuruAdd
-         return this.$store.getters.newWaheGuruAdd
-      },
-      WaheGuru1313(){
-          return this.$store.getters.abcWaheGuru('1313Wahe Guru Satname')
+      ...mapGetters({
+          waheGuru: 'newWaheGuruAdd',
+          WaheGuru1313: 'abcWaheGuru'
+      })
+    //   waheGuru(){
+    //     //  return this.$store.getters.WaheGuruAdd
+    //      return this.$store.getters.newWaheGuruAdd
+    //   },
+
+    //   WaheGuru1313(){
+    //       return this.$store.getters.abcWaheGuru('1313Wahe Guru Satname')
       }
-  }
+  
 };
 </script>
 
